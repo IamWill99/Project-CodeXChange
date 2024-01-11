6907d7810c277ed74f12b124f573428aae81c3cd
Siem Baltus <baltuss1@ad.hva.nl>
2024-01-11 10:34:25 +0100

    3456

--- src/landingspagina.ts
+++ src/landingspagina.ts
@@ -11,3 +11,55 @@ function toggleNav():void {
 document.querySelectorAll<HTMLButtonElement>(".toggle-button")
 .forEach((button: HTMLButtonElement) => button.addEventListener("click", toggleNav));
 
+class Question {
+
+    private _id: number;
+    private _userId: number;
+    private _question: string;
+    private _questionSnippet: string;
+    private _questionDate: Date;
+
+    public constructor(id: number, userId: number, question: string, questionSnippet: string, questionDate: Date) {
+        this._id = id;
+        this._userId = userId;
+        this._question = question;
+        this._questionSnippet = questionSnippet;
+        this._questionDate = questionDate;
+    }
+
+    
+    public get question(): string  {
+        return this._question;
+    }
+ 
+    public set question(question: string) {
+        this._question = question;
+    }
+
+}
+ 
+
+
+
+
+
+
+
+class Answer {
+
+    private _id: number;
+    private _userId: number;
+    private _questionId : number;
+    private _answer: string;
+    private _answerSnippet: string;
+    private _answerDate: Date;
+
+    public constructor(id: number, userId: number, questionId: number, answer: string, answerSnippet: string, answerDate: Date) {
+        this._id = id;
+        this._userId = userId;
+        this._questionId = questionId;
+        this._answer = answer;
+        this._answerSnippet = answerSnippet;
+        this._answerDate = answerDate;
+    }
+}
